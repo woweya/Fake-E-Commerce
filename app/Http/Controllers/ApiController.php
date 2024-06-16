f9496784eabfd94fa04b56b76cea96793c8e19c1
woweya <mestacca@gmail.com>
2024-06-16 21:16:38 +0200

    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+       try{
+        //Fetch the products from the API
+          $productResponse = Http::get('https://fakestoreapi.com/products?limit=6');
+          $data= $productResponse->json();
+
+          //Fetch the categories from API
+          $categoryResponse = Http::get('https://fakestoreapi.com/products/categories');
+          $categories= $categoryResponse->json();
+
+
+          return view('welcome', compact(['data', 'categories']));
+
+       }catch(\Exception $e){
+
+            $e = $e->getMessage();
+
+          return view('welcome', compact('e'));
+       }
+
+
+    }
+
+}
